f54f75093bfd47e64ef809b4bb8194ce62883d1b
Chris Camacho <cacamacho91@gmail.com>
2019-03-05 16:31:07 +0000

    started room join logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let STATE_room = undefined
-let STATE_currentUser = undefined
+let STATE_user = undefined
 let STATE_userType = undefined
 let STATE_round = undefined
 const rootEl = document.querySelector('#root')
@@ -7,16 +7,20 @@ const rootEl = document.querySelector('#root')
 document.addEventListener('DOMContentLoaded', () => {
 })
 
+if (STATE_room === undefined) {
+    drawRoomOptions()
+}
+
 
 const addNewUser = (name, roomCode) => {
     return API.joinRoom(name, roomCode)
     .then(storeUser)
 }
 
-const storeRoom = room => {
-    STATE_room = room
-    return STATE_room
-}
+const storeRoom = room => STATE_room = room
+
+const storeUser = user => STATE_user = user
+
 
 const setRoomStatus = (room, status) => {
     room.status = status
